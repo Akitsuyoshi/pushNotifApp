a12d6a280bcafb31ebaeb13decd653de22c9d6e9
Tsuyoshi Akiyama <ka090824@gmail.com>
2018-10-12 17:34:53 +0900

    Add path in server index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8001;
