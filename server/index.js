f776a004cfc3ea3b962c8ad432195cf3df04c057
Tsuyoshi Akiyama <ka090824@gmail.com>
2018-10-12 17:15:21 +0900

    Add first setup for client side

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 8001;
+
+app.use(morgan('dev'));
+app.use('/api', bodyParser.urlencoded({ extended: false }), bodyParser.json());
+
+
+// For /api/hello endpoind
+app.get('/api/hello', (req, res) => {
+  return res.send({ express: 'Hello From Express' });
+});
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', (req, res) => {
+    return res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+module.exports = app;
