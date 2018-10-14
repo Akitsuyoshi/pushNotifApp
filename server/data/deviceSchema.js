21ac9a656521e99b5d86a6e49ad41e27df10584c
Tsuyoshi Akiyama <ka090824@gmail.com>
2018-10-14 23:09:28 +0900

    Sepate each schema from model.js

---
+++ server/data/deviceSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const deviceModelSchema = new Schema({
+    name: String,
+    os: String,
+    isSubscribed: Boolean,
+    token: String,
+    registrationDate: Date,
+    notifications: [{ type: Schema.Types.ObjectId, ref: 'Notification' }],
+});
+
+const Device = mongoose.model('Device', deviceModelSchema);
+
+module.exports = Device;
