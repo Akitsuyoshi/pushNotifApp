c946773cc144dd7e33c8f2f8a9e9aa55778bc87e
Tsuyoshi Akiyama <ka090824@gmail.com>
2018-10-16 12:09:12 +0900

    connect modal component to redux

--- client/src/ModalComponent.js
+++ client/src/ModalComponent.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import './App.css';
-
+import { connect } from 'react-redux';
 import Modal from "react-responsive-modal";
 
-export default function ModalComponent(props) {
-  const { open, onCloseModal, user, pushNotification, changeContext, changeTitle, content, title } = props;
+import store from './reducers/store';
+
+import { changeModal, changeTitle, changeContent, pushNotification } from './actions';
+
+const ModalComponent = ({token, open, onCloseModal, sendNotification, updateTitle, updateContent, content, title}) => {
   return (
     <div>
       <Modal open={open} onClose={onCloseModal} center>
           <h2>Push Notification</h2>
           <form onSubmit={(e) => {
             e.preventDefault();
-            pushNotification(title, content, user);
+            store.dispatch(pushNotification(title, content, token));
           }}>
             <label>
               Title:
-              <input type="text" name="title" onChange={changeTitle} />
+              <input type="text" name="title" onChange={updateTitle} />
             </label>
             <div></div>
             <label>
               Body:
-              <input type="text" name="content" onChange={changeContext} />
+              <input type="text" name="content" onChange={updateContent} />
             </label>
               <input type="submit" value="Submit" />
           </form>
@@ -28,3 +31,32 @@ export default function ModalComponent(props) {
     </div>
   )
 }
+
+const mapStateToProps = state => ({
+  open: state.open,
+  token: state.token,
+  content: state.content,
+  title: state.title,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onCloseModal: () => {
+    dispatch(changeModal(false, null));
+  },
+  updateTitle: (e) => {
+    const newTitle = e.target.value;
+    dispatch(changeTitle(newTitle));
+  },
+  updateContent: (e) => {
+    const newContent = e.target.value;
+    dispatch(changeContent(newContent));
+  },
+  sendNotification: (title, content, token) => {
+    store.dispatch(pushNotification(title, content, token));
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ModalComponent);
