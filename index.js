dd0285e153500a49543304ef3cea980f8a582559
Tsuyoshi Akiyama <ka090824@gmail.com>
2018-10-18 00:50:34 +0900

    Change res header in server index.js

--- index.js
+++ index.js
@@ -34,10 +34,7 @@ if (cluster.isMaster) {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type');
-    res.set({
-      'content-type': 'application/json; charset=utf-8', 'Content-Length': '238', Connection: 'close', 'Cache-Control': 'no-cache',
-    });
+    
     
     next();
   });
